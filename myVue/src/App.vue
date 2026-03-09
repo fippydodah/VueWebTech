05b978bfeda27b9b43ba6f4041e20f3d263c45e7
Flippydodah <haydenbrigham@gmail.com>
2026-03-08 20:31:06 -0500

    lifecycle hooks

--- myVue/src/App.vue
+++ myVue/src/App.vue
@@ -1,146 +1,61 @@
 <template>
   <h1>{{ message }}</h1>
-
   <div class="card">
-    <h1>Scenario 1: Watch a "ref(primitive value)"</h1>
+    <h2 ref="title">This is the App component.</h2>
     <h2>Number: {{ number }}</h2>
-    <button @click="number++">Increment number by 1</button>
-  </div>
-
-  <div class="card">
-    <h1>Scenario 2: Watch a property in "ref(object)"</h1>
-    <h2>Name: {{ wizard1.name }}</h2>
-    <h2>Wand: {{ wizard1.wand }}</h2>
-    <button @click="wizard1.name = wizard1.name.toUpperCase()">
-      Change name to upper case
-    </button>
-    <button @click="changeWizard1Wand">Change wand</button>
-    <button @click="wizard1.wand.core = 'Unicorn hair'">
-      Change wand core
-    </button>
-  </div>
-
-  <div class="card">
-    <h1>Scenario 3: Watch a "ref(object)"</h1>
-    <h2>Name: {{ wizard2.name }}</h2>
-    <h2>Wand: {{ wizard2.wand }}</h2>
-    <button @click="wizard2.name = wizard2.name.toUpperCase()">
-      Change name to upper case
-    </button>
-    <button @click="wizard2.wand.core = 'Phoenix feather'">
-      Change wand core
-    </button>
-    <button @click="changeWizard">Change wizard</button>
+    <button @click="number++">Increment number by one</button>
+    <button @click="isShow = !isShow">Toggle component1</button>
+    <Component1 v-if="isShow"></Component1>
   </div>
 </template>
 
 <script setup>
-import { ref, watch } from 'vue'
+import {
+  ref,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+  watch
+} from 'vue'
 
-let message = ref('Hello, Watchers!')
+import Component1 from './Component1.vue'
 
-// Scenario 1: Watch a ref(primitive value), this is a common scenario.
+let message = ref('Hello, Lifecycle Hooks!')
+
 let number = ref(1)
+let title = ref(null)
 
-const stopWatch = watch(number, (newValue, oldValue) => {
-  // A callback function, called when number is changed.
-  console.log(
-    'Watch a ref(primitive value): number changes',
-    newValue,
-    oldValue
-  )
-  if (newValue >= 5) {
-    stopWatch()
-  }
+let isShow = ref(true)
+
+console.log('App component is setup.')
+
+onBeforeMount(() => {
+  console.log('App component is before mount.')
+  console.log(number.value) // The ref data works.
+  console.log(title.value) // Since the component has not yet been mounted, you won't have access to the component's template or DOM elements within onBeforeMount.
 })
-
-// Scenario 2: Watch a property in a ref(object)
-let wizard1 = ref({
-  id: 1001,
-  name: 'Harry Potter',
-  house: 'Gryffindor',
-  age: 17,
-  wand: {
-    core: 'Phoenix feather',
-    wood: 'Holly'
-  }
+onMounted(() => {
+  console.log('App component is mounted.')
+  console.log(title.value) // This works once the component is mounted.
 })
-
-function changeWizard1Wand() {
-  wizard1.value.wand = {
-    core: 'Dragon heartstring',
-    wood: 'Vine'
-  }
-}
-
-watch(
-  () => wizard1.value.name,
-  (newValue, oldValue) => {
-    console.log(
-      'Watch a property in a ref(object): wizard1 name changes',
-      newValue,
-      oldValue
-    )
-  }
-)
-
-watch(
-  () => wizard1.value.wand,
-  (newValue, oldValue) => {
-    console.log(
-      'Watch a property in a ref(object): wizard1 wand changes',
-      newValue,
-      oldValue
-    )
-  },
-  { deep: true }
-)
-
-// Scenario 3: Watch a ref(object)
-let wizard2 = ref({
-  id: 1003,
-  name: 'Ron Weasley',
-  house: 'Gryffindor',
-  age: 17,
-  wand: {
-    core: 'Unicorn hair',
-    wood: 'Willow'
-  }
+onBeforeUpdate(() => {
+  console.log('App component is before update.')
 })
-
-function changeWizard() {
-  wizard2.value = {
-    id: 1002,
-    name: 'Hermione Granger',
-    house: 'Gryffindor',
-    age: 17,
-    wand: {
-      core: 'Dragon heartstring',
-      wood: 'Vine'
-    }
-  }
-}
-
-watch(
-  wizard2,
-  (newValue, oldValue) => {
-    console.log('Watch a ref(object): wizard2 changes', newValue, oldValue)
-  },
-  { deep: true }
-)
-
-// Scenario 4: Watch an array of ref(primitive value), a property of a ref(object), and a ref(object)
-watch(
-  [number, () => wizard1.value.name, wizard2],
-  (newValue, oldValue) => {
-    console.log(
-      'Watch an array of ref(primitive value), a property of a ref(object), and a ref(object)'
-    )
-  },
-  {
-    deep: true
-  }
-)
+onUpdated(() => {
+  console.log('App component is updated.')
+})
+onBeforeUnmount(() => {
+  console.log('App component is before unmount.')
+})
+onUnmounted(() => {
+  console.log('App component is unmounted.')
+})
+watch(number, () => {
+  console.log('number changes!')
+})
 </script>
 
 <style scoped>
